4c7fc7a4c4c73e62b3c24e6a07e28b80ad351f9f
luis-jmtz <luis.jmtz2002@gmail.com>
2025-11-27 14:06:25 -0600

    basic player_character class done

---
+++ Player_Character.py
@@ -0,0 +1,13 @@
+from Creatures import Creature
+
+class Player_Character(Creature):
+    def __init__(self):
+        super().__init__(self)
+        self.pc_class = 0 # id of player class
+        self.species = 0 # id of player species
+        self.vitality = 0 # effectively extra health a player character has after their hit_points have been depleted
+        
+        self.skill_points = 0
+        self.expertise_points = 0
+        self.skills = None # a dataframe that stores player skill information
+        
