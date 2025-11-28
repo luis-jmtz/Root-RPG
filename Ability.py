62a4c3b022289d07b13e7bc6cc88ac149c92b4a8
luis-jmtz <luis.jmtz2002@gmail.com>
2025-11-28 13:20:01 -0600

    ability class done

---
+++ Ability.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Ability():
+    def __init__(self):
+        self.id = 0 
+        self.description = ""
+        self.pp = 0 # how much the ability contributes to the creauture's difficulty
+
+class Species_Trait(Ability):
+    def __init__(self):
+        super().__init__()
+        self.species_id = 0 #species id
+
+class Class_Ability(Ability):
+    def __init__(self):
+        super().__init__()
+        self.class_id = 0
