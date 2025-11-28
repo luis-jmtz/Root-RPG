ca0fdbf7f9edc70f0c80c2d34c45577f6cfc2fd6
luis-jmtz <luis.jmtz2002@gmail.com>
2025-11-28 13:23:15 -0600

    initialized pp calculator for later use

---
+++ power_point_calc.py
@@ -0,0 +1,8 @@
+'''
+this file will be used to calculate the amount of power points
+an ability, manuever, or equipment provides.
+
+All power points for a creature will be calculated to determine it Diffiuclty Level
+'''
+
+
