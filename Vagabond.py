f86528c052cac858029c41789835872fcf3eaf27
luis-jmtz <luis.jmtz2002@gmail.com>
2025-12-05 16:16:30 -0600

    updated vagabond to match proficiency system changes

--- Vagabond.py
+++ Vagabond.py
@@ -6,9 +6,10 @@ class Vagabond(Creature):
         self.pc_class = 0 # id of player class
         self.species = 0 # id of player species
         self.vitality = 0 # effectively extra health a player character has after their hit_points have been depleted
+
+        self.equipment_proficiencies = [] # equipment proficiencies (abilities)
         
-        self.skill_points = 0
-        self.expertise_points = 0
+        self.skill_proficiencies = [] # list of skills (id) that a player character has proficiency in
         self.skills = None # a dataframe that stores player skill information
 
         self.quiver = [] # holds ammunition, only used when a player has a quiver
