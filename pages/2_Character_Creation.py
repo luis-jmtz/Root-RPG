394298f6518691f0b3c51b64a1d9883cb18aca05
luis-jmtz <luis.jmtz2002@gmail.com>
2026-02-11 21:59:41 -0600

    changed character creation to have linux path for published app

--- pages/2_Character_Creation.py
+++ pages/2_Character_Creation.py
@@ -1,7 +1,22 @@
 import streamlit as st
+from pathlib import Path
 
 
-cc_path = r"Rules_Text\Character_Creation.md"
+# cc_path = r"Rules_Text\Character_Creation.md"
+
+# with open(cc_path, 'r', encoding='utf-8') as f:
+#     cc_text = f.read()
+
+# st.markdown(cc_text)
+
+
+
+# Get the directory where this script is located
+current_dir = Path(__file__).parent
+# Go up one level to the root directory (since this file is in the pages folder)
+root_dir = current_dir.parent
+
+cc_path = root_dir / 'Rules_Text' / 'Character_Creation.md'
 
 with open(cc_path, 'r', encoding='utf-8') as f:
     cc_text = f.read()
